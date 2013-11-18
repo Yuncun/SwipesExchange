b5cdbc3615d733243fd1b84e2b01d7033ad5005a
kplutchak <kplutchak@ucla.edu>
2013-11-17 17:31:23 -0800

    Redo previous commit.

---
+++ Android/src/com/example/tabsfinal/BuyListing.java
@@ -0,0 +1,5 @@
+package com.example.tabsfinal;
+
+public class BuyListing extends Listing{
+
+}
---
+++ Android/src/com/example/tabsfinal/SellListing.java
@@ -0,0 +1,19 @@
+package com.example.tabsfinal;
+
+
+
+public class SellListing extends Listing{
+
+	private double price;
+	
+	public void setPrice(double price) {
+		
+		this.price = price;
+	}
+	
+	public double getPrice() {
+		return this.price;
+		
+	}
+	
+}
